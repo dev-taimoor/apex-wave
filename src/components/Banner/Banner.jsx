ead3679ea8b498a2179e2fdac7969db0182e6f55
dev-taimoor <taimoor220297@gmail.com>
2024-08-08 19:24:49 +0500

    Products removed

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,10 @@
 import { Col, Container, Row } from "react-bootstrap";
-import productBg from "../../Images/table.jpg";
+// import productBg from "../../Images/table.jpg";
 import "./banner.css";
 const Banner = ({title}) => {
     return ( 
         <div className="image-container">
-            <img src={productBg} alt="Product-bg" />
+            {/* <img src={} alt="Product-bg" /> */}
             <div className="overlay">
                 <Container>
                     <Row>
